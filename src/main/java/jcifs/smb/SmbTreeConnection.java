9512777de9b98f3d684a757d28c70135bfeafa23
Ryan Lambeth <rlambeth@idauto.net>
2025-10-08 10:45:58 -0400

    [NOISSUE]: Revert change to resolveDfs0

--- src/main/java/jcifs/smb/SmbTreeConnection.java
+++ src/main/java/jcifs/smb/SmbTreeConnection.java
@@ -716,8 +716,8 @@ class SmbTreeConnection {
                         if ( log.isDebugEnabled() ) {
                             log.debug(String.format("Need to adjust request path %s (full: %s) -> %s", rpath, rfullpath, dr));
                         }
+                        String dunc = loc.handleDFSReferral(dr, rpath);
                         if ( request != null ) {
-                            String dunc = loc.handleDFSReferral(dr, rpath);
                             request.setPath(dunc);
                         }
                         return loc;
@@ -743,8 +743,8 @@ class SmbTreeConnection {
                     log.debug("Resolved " + rfullpath + " -> " + dr);
                 }
 
+                String dunc = loc.handleDFSReferral(dr, rpath);
                 if ( request != null ) {
-                    String dunc = loc.handleDFSReferral(dr, rpath);
                     request.setPath(dunc);
                 }
 
